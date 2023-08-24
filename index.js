6910c95fdc482ef466fa6be4b4ce42722ea4e802
Ahmed Maqbool <ahmedmaqx11@gmail.com>
2023-08-24 17:17:11 +0000

    added mongoose and made notes schema

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const HyperExpress = require('hyper-express')
+const mongoose = require('mongoose');
 const app = new HyperExpress.Server();
-const PORT = 3000 || process.env.PORT
+const PORT = 3000 || process.env.PORT;
+const { Notes } = require('./models/notesSchema');
+
 
 app.get('/', (req, res) => {
-        res.json({ 'Come_On': "Hello From Hyper Express Container" })
+        res.json({ 'helllo': "Hello From Hyper Express Container" })
 })
 
-app.listen(PORT)
-        .then((socket) => console.log(`Server started on ${PORT}`))
-        .catch((error) => console.log(`Failer To Start Server on ${PORT}`))
+app.post('/create', async (req, res) => {
+        try {
+                const { title, body } = await req.json()
+                const dbResponse = await Notes.create({ title, body })
+                res.status(201).send("Created")
+        } catch (error) {
+                res.status(500).send("Error")
+        }
+})
+
+mongoose.connect(process.env.MONGODB_URI)
+        .then(() => {
+                console.log('db connected')
+                app.listen(PORT)
+                        .then(() => console.log(`Server Running On ${PORT}`))
+                        .catch(() => console.log(`Server Failed To Start On ${PORT}`))
+        })
---
+++ models/notesSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const { randomUUID } = require('crypto')
+const { Schema } = mongoose;
+
+const notesSchema = new Schema({
+    note_id: {
+        type: String,
+        default: () => randomUUID()
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true })
+
+const Notes = mongoose.model('Notes', notesSchema);
+
+module.exports = { Notes }
